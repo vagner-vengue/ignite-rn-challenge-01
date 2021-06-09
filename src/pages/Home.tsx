3a5a1e200bc2291b18ff4cb9281830f3bd7f1898
Vagner Vengue <vagnervengue@users.noreply.github.com>
2021-06-09 20:26:44 -0300

    TODO updates.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,32 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task if it's not empty
+
+    if (!newTaskTitle)
+      return;
+    
+    setTasks([...tasks, { id: new Date().getTime(), title: newTaskTitle, done: false } ]);
   }
 
   function handleMarkTaskAsDone(id: number) {
     //TODO - mark task as done if exists
+
+    let item = tasks.find(item => item.id === id);
+
+    if (item)
+      item.done = ! item.done;
+    
+    setTasks([...tasks]);
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+
+    setTasks(old => old.filter(item => item.id !== id));
   }
 
   return (
